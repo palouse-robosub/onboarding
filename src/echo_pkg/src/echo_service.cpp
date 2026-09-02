b63a0ad73ee23145759f2e7c4b293e8ec36aeff8
Andy Sorge <124544413+andy-sorge@users.noreply.github.com>
2026-09-02 00:26:26 -0700

    chore: formatting

--- src/echo_pkg/src/echo_service.cpp
+++ src/echo_pkg/src/echo_service.cpp
@@ -3,13 +3,12 @@
 
 class EchoService : public rclcpp::Node {
   public:
-    EchoService() :
-      Node("echo_service") {
+    EchoService() : Node("echo_service") {
         echo_service_ = this->create_service<onboarding_msgs::srv::EchoString>(
             "echo_string", std::bind(
-                                &EchoService::echo_callback, this,
-                                std::placeholders::_1, std::placeholders::_2
-                            )
+                               &EchoService::echo_callback, this,
+                               std::placeholders::_1, std::placeholders::_2
+                           )
         );
     }
 
@@ -17,14 +16,13 @@ class EchoService : public rclcpp::Node {
     rclcpp::Service<onboarding_msgs::srv::EchoString>::SharedPtr echo_service_;
 
     void echo_callback(
-        const std::shared_ptr<onboarding_msgs::srv::EchoString::Request> request,
+        const std::shared_ptr<onboarding_msgs::srv::EchoString::Request>
+                                                                    request,
         std::shared_ptr<onboarding_msgs::srv::EchoString::Response> response
     ) {
-      RCLCPP_INFO(
-        get_logger(),
-        "Echo request recieved: %s",
-        request->data.c_str()
-      );
+        RCLCPP_INFO(
+            get_logger(), "Echo request recieved: %s", request->data.c_str()
+        );
     }
 };
 
