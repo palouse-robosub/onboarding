22c389649b21aee68bb8b4fce338f01e4ad7c085
Cole Wilson <colewilson.main@gmail.com>
2026-09-02 08:26:25 +0000

    feat: add more example packages

---
+++ src/onboarding_example/src/example_publisher.cpp
@@ -0,0 +1,13 @@
+// includes
+
+class ExamplePublisher {
+  public:
+    ExamplePublisher() {
+        // constructor
+    }
+};
+
+int main(int argc, char* argv[]) {
+    // setup and spin
+    return 0;
+}
