14623308f9ab771e5b2e0bb3ad489c588cd36d2d
Dylan Dean <230458421+dyloniuss@users.noreply.github.com>
2026-09-03 01:23:28 +0000

    feat: my publisher and subscriber

---
+++ src/dylan_pkg/src/example_subscriber.cpp
@@ -0,0 +1,13 @@
+// includes
+
+class ExampleSubscriber {
+  public:
+    ExampleSubscriber() {
+        // constructor
+    }
+};
+
+int main(int argc, char* argv[]) {
+    // setup and spin
+    return 0;
+}
