127d549fecf11524df4262c8cd9be6779e311da1
GoldenGuy1000 <GoldenGuy1000Contact@gmail.com>
2026-09-04 00:34:02 +0000

    feat: oliver publisher and subscriber now use the custom data type

--- src/oliver_pkg/src/oliver_publisher.cpp
+++ src/oliver_pkg/src/oliver_publisher.cpp
@@ -2,42 +2,43 @@
 #include <memory>
 #include <string>
 
+#include "onboarding_msgs/msg/oliver_double.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses a fancy C++11 lambda
-* function to shorten the callback syntax, at the expense of making the
-* code somewhat more difficult to understand at first glance. */
+ * function to shorten the callback syntax, at the expense of making the
+ * code somewhat more difficult to understand at first glance. */
 
-class OliverPublisher : public rclcpp::Node
-{
-public:
-  OliverPublisher()
-  : Node("oliver_publisher"), count_(0)
-  {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("oliver_topic", 10);
-    auto timer_callback =
-      [this]() -> void {
-        auto message = std_msgs::msg::String();
-        message.data = "Hello, everyone! " + std::to_string(this->count_++);
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-        this->publisher_->publish(message);
-      };
-    timer_ = this->create_wall_timer(500ms, timer_callback);
-  }
+class OliverPublisher : public rclcpp::Node {
+  public:
+    OliverPublisher() : Node("oliver_publisher"), count_(0) {
+        publisher_ = this->create_publisher<onboarding_msgs::msg::OliverDouble>(
+            "oliver_topic", 10
+        );
+        auto timer_callback = [this]() -> void {
+            auto message = onboarding_msgs::msg::OliverDouble();
 
-private:
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+            message.my_glorious_double        = std::pow(2, count_);
+            message.my_second_glorious_double = std::pow(2, -count_);
+            count_++;
+
+            this->publisher_->publish(message);
+        };
+        timer_ = this->create_wall_timer(2000ms, timer_callback);
+    }
+
+  private:
+    rclcpp::TimerBase::SharedPtr                                     timer_;
+    rclcpp::Publisher<onboarding_msgs::msg::OliverDouble>::SharedPtr publisher_;
+    double                                                           count_;
 };
 
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<OliverPublisher>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<OliverPublisher>());
+    rclcpp::shutdown();
+    return 0;
 }
--- src/oliver_pkg/src/oliver_subscriber.cpp
+++ src/oliver_pkg/src/oliver_subscriber.cpp
@@ -1,30 +1,46 @@
 #include <memory>
 
+#include "onboarding_msgs/msg/oliver_double.hpp"
+#include "onboarding_msgs/srv/echo_string.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-class OliverSubscriber : public rclcpp::Node
-{
-public:
-  OliverSubscriber()
-  : Node("oliver_subscriber")
-  {
-    auto topic_callback =
-      [this](std_msgs::msg::String::UniquePtr msg) -> void {
-        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
-      };
-    subscription_ =
-      this->create_subscription<std_msgs::msg::String>("oliver_topic", 10, topic_callback);
-  }
+class OliverSubscriber : public rclcpp::Node {
+  public:
+    OliverSubscriber() : Node("oliver_subscriber") {
+        std::shared_ptr<rclcpp::Node> node =
+            rclcpp::Node::make_shared("oliver_onboarding_echo_client");
+        rclcpp::Client<onboarding_msgs::srv::EchoString>::SharedPtr client =
+            node->create_client<onboarding_msgs::srv::EchoString>(
+                "echo_string"
+            );
+        auto topic_callback =
+            [this, client](
+                onboarding_msgs::msg::OliverDouble::UniquePtr msg
+            ) -> void {
+            auto echo_string =
+                std::make_shared<onboarding_msgs::srv::EchoString::Request>();
+            std::stringstream output{};
+            output << "Oliver's doubles are diverging: "
+                   << msg->my_glorious_double << " "
+                   << msg->my_second_glorious_double;
+            echo_string->data = output.str();
+            client->async_send_request(echo_string);
+        };
+        subscription_ =
+            this->create_subscription<onboarding_msgs::msg::OliverDouble>(
+                "oliver_topic", 10, topic_callback
+            );
+    }
 
-private:
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  private:
+    rclcpp::Subscription<onboarding_msgs::msg::OliverDouble>::SharedPtr
+        subscription_;
 };
 
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<OliverSubscriber>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<OliverSubscriber>());
+    rclcpp::shutdown();
+    return 0;
 }
