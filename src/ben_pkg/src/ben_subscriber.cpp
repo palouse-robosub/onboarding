0a933c2f9299da4fece44405eb04c8a4e78fa261
Ben Rao <benvrao@gmail.com>
2026-09-09 16:37:31 -0700

    Ben Package
    * feat: Onboarding complete
    
    * fix: updated ReadMe file
    
    * fix: ran formatter
    
    * fix: in subscriber, uses this instead of creating a new node; moved client initialization outside of the callback
    
    * fix: ran formatter
    
    * fix: removed and edit some comments
    
    * fix: ran formatter
    
    * fix: edited package.xml with updated information

---
+++ src/ben_pkg/src/ben_subscriber.cpp
@@ -0,0 +1,48 @@
+#include <memory>
+
+#include "onboarding_msgs/msg/ben_message.hpp"
+#include "onboarding_msgs/srv/echo_string.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+class BenSubscriber : public rclcpp::Node {
+  public:
+    rclcpp::Subscription<onboarding_msgs::msg::BenMessage>::SharedPtr
+        subscription_;
+
+    BenSubscriber() : Node("ben_subscriber") {
+        // creating a service client within the subscriber node which we call
+        // "echo_string"
+        client = this->create_client<onboarding_msgs::srv::EchoString>(
+            "echo_string"
+        );
+
+        auto topic_callback =
+            [this](onboarding_msgs::msg::BenMessage::UniquePtr msg) -> void {
+            // Next, create a request
+            auto message =
+                std::make_shared<onboarding_msgs::srv::EchoString::Request>();
+            message->data =
+                "Ben's uint32 is counting up: " + std::to_string(msg->data);
+
+            // Next, send the request
+            auto result = client->async_send_request(message);
+
+            // We do not use spin_until_future_complete here because because
+            // main spun the node already
+        };
+        subscription_ =
+            this->create_subscription<onboarding_msgs::msg::BenMessage>(
+                "ben_topic", 10, topic_callback
+            );
+    }
+
+  private:
+    rclcpp::Client<onboarding_msgs::srv::EchoString>::SharedPtr client;
+};
+
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<BenSubscriber>());
+    rclcpp::shutdown();
+    return 0;
+}
