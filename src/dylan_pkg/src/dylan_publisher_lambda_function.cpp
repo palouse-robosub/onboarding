71af65f71872d78d5fbd559b80c99741a106564b
Dylan Dean <230458421+dyloniuss@users.noreply.github.com>
2026-09-09 08:42:10 +0000

    feat: made dylan_pkg publisher/subscriber with data type and echo_string client

--- src/dylan_pkg/src/dylan_publisher_lambda_function.cpp
+++ src/dylan_pkg/src/dylan_publisher_lambda_function.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float32.hpp"
 
 using namespace std::chrono_literals;
 
@@ -14,22 +14,21 @@ using namespace std::chrono_literals;
 class MinimalPublisher : public rclcpp::Node {
   public:
     MinimalPublisher() : Node("minimal_publisher"), count_(0) {
-        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        publisher_ =
+            this->create_publisher<std_msgs::msg::Float32>("topic", 10);
         auto timer_callback = [this]() -> void {
-            auto message = std_msgs::msg::String();
-            message.data = "Hello, world! " + std::to_string(this->count_++);
-            RCLCPP_INFO(
-                this->get_logger(), "Publishing: '%s'", message.data.c_str()
-            );
+            auto message = std_msgs::msg::Float32();
+            message.data = static_cast<float>(this->count_++);
+            RCLCPP_INFO(this->get_logger(), "Publishing: '%f'", message.data);
             this->publisher_->publish(message);
         };
-        timer_ = this->create_wall_timer(500ms, timer_callback);
+        timer_ = this->create_wall_timer(2000ms, timer_callback);
     }
 
   private:
-    rclcpp::TimerBase::SharedPtr                        timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t                                              count_;
+    rclcpp::TimerBase::SharedPtr                         timer_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_;
+    size_t                                               count_;
 };
 
 int main(int argc, char* argv[]) {
--- src/dylan_pkg/src/dylan_subscriber_lambda_function.cpp
+++ src/dylan_pkg/src/dylan_subscriber_lambda_function.cpp
@@ -1,22 +1,40 @@
 #include <memory>
 
+#include "onboarding_msgs/srv/echo_string.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float32.hpp"
 
 class MinimalSubscriber : public rclcpp::Node {
   public:
     MinimalSubscriber() : Node("minimal_subscriber") {
+        client_ = this->create_client<onboarding_msgs::srv::EchoString>(
+            "/echo_string"
+        );
+
         auto topic_callback =
-            [this](std_msgs::msg::String::UniquePtr msg) -> void {
-            RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+            [this](std_msgs::msg::Float32::UniquePtr msg) -> void {
+            RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->data);
+            std::string summary =
+                "Dylan received a float: " + std::to_string(msg->data);
+            auto request =
+                std::make_shared<onboarding_msgs::srv::EchoString::Request>();
+            request->data = summary;
+            if (!client_->wait_for_service(std::chrono::seconds(1))) {
+                RCLCPP_WARN(
+                    this->get_logger(), "/echo_string service not available"
+                );
+                return;
+            }
+            client_->async_send_request(request);
         };
-        subscription_ = this->create_subscription<std_msgs::msg::String>(
+        subscription_ = this->create_subscription<std_msgs::msg::Float32>(
             "topic", 10, topic_callback
         );
     }
 
   private:
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr     subscription_;
+    rclcpp::Client<onboarding_msgs::srv::EchoString>::SharedPtr client_;
 };
 
 int main(int argc, char* argv[]) {
